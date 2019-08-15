aad19943a4d0ef57d370cbc97068c238d96996b3
chunyu1202 <chunyu1202@gmail.com>
2019-08-15 14:17:55 +0800

    Add more comments to make code clear

--- main_battery.c
+++ main_battery.c
@@ -332,8 +332,48 @@ static void btn_state_handler(void * p_context)
     advertising_start();
 }
 
+void battery_measure_init(){
+
+/*
+    NRF_CLOCK->TASKS_HFCLKSTART = 1;
+    
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
+    
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+*/
+    // Configure SAADC singled-ended channel, Iternal referecne (0.6V) and 1/6 gain.
+    NRF_SAADC->CH[0].CONFIG = 
+    (SAADC_CH_CONFIG_GAIN_Gain1_6    << SAADC_CH_CONFIG_GAIN_Pos) |
+    (SAADC_CH_CONFIG_MODE_SE         << SAADC_CH_CONFIG_MODE_Pos) |
+    (SAADC_CH_CONFIG_REFSEL_Internal << SAADC_CH_CONFIG_REFSEL_Pos) |
+    (SAADC_CH_CONFIG_RESN_Bypass     << SAADC_CH_CONFIG_RESN_Pos) |
+    (SAADC_CH_CONFIG_RESP_Bypass     << SAADC_CH_CONFIG_RESP_Pos) |
+    (SAADC_CH_CONFIG_TACQ_3us        << SAADC_CH_CONFIG_TACQ_Pos);
+
+    NRF_SAADC->CH[0].PSELP = SAADC_CH_PSELP_PSELP_VDD << SAADC_CH_PSELP_PSELP_Pos;
+    NRF_SAADC->CH[0].PSELN = SAADC_CH_PSELN_PSELN_NC << SAADC_CH_PSELN_PSELN_Pos;
+
+    // Configure the SAADC resolution.
+    NRF_SAADC->RESOLUTION = SAADC_RESOLUTION_VAL_14bit << SAADC_RESOLUTION_VAL_Pos;
+
+    // Configure result to be put in RAM at the location of "result" variable.
+    NRF_SAADC->RESULT.MAXCNT = 1;
+    NRF_SAADC->RESULT.PTR = (uint32_t)&result;
+
+    // No automatic sampling, will trigger with TASKS_SAMPLE
+    NRF_SAADC->SAMPLERATE = SAADC_SAMPLERATE_MODE_Task << SAADC_SAMPLERATE_MODE_Pos;
+
+    // Enable SAADC (would capture analog pins if they were used in CH[0].PSELP)
+    NRF_SAADC->ENABLE = SAADC_ENABLE_ENABLE_Enabled << SAADC_ENABLE_ENABLE_Pos;
+
+
+    ret_code_t err_code = app_timer_start(battery_measure_timer, APP_TIMER_TICKS(1800000), NULL);
+    APP_ERROR_CHECK(err_code);
+}
+
 static void battery_measure_voltage(){
 
+    // Calibrate the SAADC (only needs to be done once in a while)
     NRF_SAADC->TASKS_CALIBRATEOFFSET = 1;
     
     while (NRF_SAADC->EVENTS_CALIBRATEDONE == 0);
@@ -342,12 +382,14 @@ static void battery_measure_voltage(){
     
     while (NRF_SAADC->STATUS == (SAADC_STATUS_STATUS_Busy <<SAADC_STATUS_STATUS_Pos));
 
+    // Start the SAADC and wait for the started event.
     NRF_SAADC->TASKS_START = 1;
     
     while (NRF_SAADC->EVENTS_STARTED == 0);
     
     NRF_SAADC->EVENTS_STARTED = 0;
 
+    // Do a SAADC sample, will put the result in the configured RAM buffer.
     NRF_SAADC->TASKS_SAMPLE = 1;
     
     while (NRF_SAADC->EVENTS_END == 0);
@@ -357,6 +399,7 @@ static void battery_measure_voltage(){
     precise_result = (float)result / 4551.1f;
     BATT_LVL = ((uint8_t)(precise_result*10));
 
+    // Stop the SAADC, since it is not used anymore.
     NRF_SAADC->TASKS_STOP = 1;
     
     while (NRF_SAADC->EVENTS_STOPPED == 0);
@@ -460,39 +503,6 @@ static void set_tx_power()
 
     // Accepted values are -40, -20, -16, -12, -8, -4, 0, 4 (dbm)
     err_code = sd_ble_gap_tx_power_set(BLE_GAP_TX_POWER_ROLE_ADV, NULL, 0);
-    APP_ERROR_CHECK(err_code);
-}
-
- void battery_measure_init(){
-
-    NRF_CLOCK->TASKS_HFCLKSTART = 1;
-    
-    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
-    
-    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-
-
-    NRF_SAADC->CH[0].CONFIG = (SAADC_CH_CONFIG_GAIN_Gain1_6    << SAADC_CH_CONFIG_GAIN_Pos) |
-    (SAADC_CH_CONFIG_MODE_SE         << SAADC_CH_CONFIG_MODE_Pos) |
-    (SAADC_CH_CONFIG_REFSEL_Internal << SAADC_CH_CONFIG_REFSEL_Pos) |
-    (SAADC_CH_CONFIG_RESN_Bypass     << SAADC_CH_CONFIG_RESN_Pos) |
-    (SAADC_CH_CONFIG_RESP_Bypass     << SAADC_CH_CONFIG_RESP_Pos) |
-    (SAADC_CH_CONFIG_TACQ_3us        << SAADC_CH_CONFIG_TACQ_Pos);
-
-    NRF_SAADC->CH[0].PSELP = SAADC_CH_PSELP_PSELP_VDD << SAADC_CH_PSELP_PSELP_Pos;
-    NRF_SAADC->CH[0].PSELN = SAADC_CH_PSELN_PSELN_NC << SAADC_CH_PSELN_PSELN_Pos;
-
-    NRF_SAADC->RESOLUTION = SAADC_RESOLUTION_VAL_14bit << SAADC_RESOLUTION_VAL_Pos;
-
-    NRF_SAADC->RESULT.MAXCNT = 1;
-    NRF_SAADC->RESULT.PTR = (uint32_t)&result;
-
-    NRF_SAADC->SAMPLERATE = SAADC_SAMPLERATE_MODE_Task << SAADC_SAMPLERATE_MODE_Pos;
-
-    NRF_SAADC->ENABLE = SAADC_ENABLE_ENABLE_Enabled << SAADC_ENABLE_ENABLE_Pos;
-
-
-    ret_code_t err_code = app_timer_start(battery_measure_timer, APP_TIMER_TICKS(1800000), NULL);
     APP_ERROR_CHECK(err_code);
 }
 
