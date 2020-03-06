6d64537e5f67a713043d365ea174581a893e8e63
Jimmy Lai <chunyu1202@gmail.com>
2020-03-06 15:42:29 +0800

    Refine the codes

--- nRF5_SDK-15.0.0_a53641a/examples/ble_peripheral/BeDITech_ble_app_beacon/pca10040/s132/ses/Output/ble_app_beacon_pca10040_s132 Release/Obj/ses_nRF_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_beacon_pca10040_s132\ Release/Obj/ses_nRF_Startup.o: \
- C:\Users\openISDM\Documents\GitHub\nRF5_SDK_15.0.0_a53641a\nRF5_SDK_15.0.0_a53641a\modules\nrfx\mdk\ses_nRF_Startup.s
